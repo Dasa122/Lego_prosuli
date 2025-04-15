2a35967975d75c0445bde8129e7deaf2548aacad
Tamás Vince Kornél <vince@tvk.lol>
2025-04-15 10:46:42 +0200

    fasza

--- jklhjkv.py
+++ jklhjkv.py
@@ -18,32 +18,38 @@ motor_r = Motor(Port.C)
 def read_sensors():
     return [x.reflection() for x in sensors]
 
-SPEED_BASE = 200
+# this works pretty well with 3x mult
+#SPEED_BASE = 350
+
+SPEED_BASE = 500
+
+# og was 25
+THRESHOLD = 35
 
 while True:
     sensor_data = read_sensors()
-    leftleft_detect = sensor_data[0] < 25
-    rightright_detect = sensor_data[3] < 25
-    left_detect = sensor_data[1] < 25
-    right_detect = sensor_data[2] < 25
+    leftleft_detect = sensor_data[0] < THRESHOLD
+    rightright_detect = sensor_data[3] < THRESHOLD
+    left_detect = sensor_data[1] < THRESHOLD
+    right_detect = sensor_data[2] < THRESHOLD
 
 
     # If left sensor detects line, turn right until right detects too
     if leftleft_detect or left_detect and not rightright_detect or right_detect:
-        motor_l.run(SPEED_BASE)
-        motor_r.run(SPEED_BASE * 4)
+        motor_l.run(SPEED_BASE * 0.2)
+        motor_r.run(SPEED_BASE * 3)
         while True:
             sensor_data = read_sensors()
-            if sensor_data[3] < 25:  # Right sensor sees line
+            if sensor_data[3] < THRESHOLD:  # Right sensor sees line
                 break
 
     # If right sensor detects line, turn left until left detects too
     elif rightright_detect or right_detect and not leftleft_detect or left_detect:
-        motor_l.run(SPEED_BASE * 4)
-        motor_r.run(SPEED_BASE)
+        motor_l.run(SPEED_BASE * 3)
+        motor_r.run(SPEED_BASE * 0.2)
         while True:
             sensor_data = read_sensors()
-            if sensor_data[0] < 25:  # Left sensor sees line
+            if sensor_data[0] < THRESHOLD:  # Left sensor sees line
                 break
 
     # If both or none detect, go straight
@@ -51,5 +57,5 @@ while True:
         motor_l.run(SPEED_BASE)
         motor_r.run(SPEED_BASE)
 
-    print("Sensor reflections:", sensor_data)
-    wait(10)
+    # print("Sensor reflections:", sensor_data)
+    wait(5)
